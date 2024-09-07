aedb961f2a5774cc8352136696090239b9465870
Sumanth <89512217+sumanth-appu@users.noreply.github.com>
2024-09-07 16:22:42 +0530

    Side-navbar update

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
-var app = angular.module('dashboardApp', []);
+  // Initialize Materialize components
+  document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(elems);
+  });
 
-app.controller('DashboardController', function($scope) {
+  // Initialize AngularJS app and controller
+  var app = angular.module("dashboardApp", []);
+
+  app.controller("DashboardController", function ($scope) {
     $scope.cards = [
-        { title: 'Progress Report', icon: 'assessment', color: 'red lighten-3', link: '#', progress: 70 },
-        { title: 'Reports', icon: 'insert_drive_file', color: 'purple lighten-3', link: '#', progress: 80 },
-        { title: 'Admin Ops', icon: 'person', color: 'deep-purple lighten-3', link: '#', progress: 60 },
-        { title: 'Indexing QC', icon: 'check_box', color: 'cyan lighten-3', link: '#', progress: 50 },
-        { title: 'Installation QC', icon: 'electric_meter', color: 'blue lighten-3', link: '#', progress: 90 },
-        { title: 'Maintenance QC', icon: 'settings', color: 'teal lighten-3', link: '#', progress: 85 },
-        { title: 'Inventory', icon: 'storage', color: 'light-blue lighten-3', link: '#', progress: 75 },
-        
+      {
+        title: "Progress Report",
+        icon: "assessment",
+        color: "red lighten-3",
+        link: "#",
+        progress: 70,
+      },
+      {
+        title: "Reports",
+        icon: "insert_drive_file",
+        color: "purple lighten-3",
+        link: "#",
+        progress: 80,
+      },
+      {
+        title: "Admin Ops",
+        icon: "person",
+        color: "deep-purple lighten-3",
+        link: "#",
+        progress: 60,
+      },
+      {
+        title: "Indexing QC",
+        icon: "check_box",
+        color: "cyan lighten-3",
+        link: "#",
+        progress: 50,
+      },
+      {
+        title: "Installation QC",
+        icon: "electric_meter",
+        color: "blue lighten-3",
+        link: "#",
+        progress: 90,
+      },
+      {
+        title: "Maintenance QC",
+        icon: "settings",
+        color: "teal lighten-3",
+        link: "#",
+        progress: 85,
+      },
+      {
+        title: "Inventory",
+        icon: "storage",
+        color: "light-blue lighten-3",
+        link: "#",
+        progress: 75,
+      },
     ];
-});
+  });
